82ae3693b48a140565b6e8eb86127ea69210bc9a
jenaro fv <jen4r0@gmail.com>
2024-11-15 09:04:43 -0600

    Adds interactive portfolio buttons

--- themeswitch.js
+++ themeswitch.js
@@ -4,6 +4,7 @@ const themeSwitcher = document.getElementById("themeSwitch");
 const logos = document.querySelectorAll(".logo");
 const textLinks = document.querySelectorAll(".textlink");
 const buttons = document.querySelectorAll(".button");
+const projects = document.querySelectorAll(".entry");
 
 const switchThemes = () => {
     theBody.classList.toggle("darkBody");
@@ -16,6 +17,18 @@ const switchThemes = () => {
     for (const button of buttons) {
         button.classList.toggle("darkButton");
     }
+    for (const project of projects) {
+        project.classList.toggle("darkEntry");
+    }
+}
+
+function showProjects() {
+    this.classList.toggle("show");
+    this.nextElementSibling.classList.toggle("show");
 }
 
 themeSwitcher.addEventListener("click", switchThemes);
+
+for (const project of projects) {
+    project.addEventListener("click", showProjects);
+}
