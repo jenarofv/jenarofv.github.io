44f3671775422ab0b9cdf5ec5c526b7496ad8874
jenaro fv <jen4r0@gmail.com>
2025-05-09 08:35:34 -0600

    removes theme switching button
    Drops dark classes in favor of @media (prefers-color-scheme: dark)

--- themeswitch.js
+++ themeswitch.js
@@ -1,33 +1,10 @@
-const theBody = document.querySelector("body");
-const bodyStyle = window.getComputedStyle(theBody);
-const themeSwitcher = document.getElementById("themeSwitch");
-const logos = document.querySelectorAll(".logo");
-const textLinks = document.querySelectorAll(".textlink");
-const buttons = document.querySelectorAll(".button");
 const projects = document.querySelectorAll(".entry");
-
-const switchThemes = () => {
-    theBody.classList.toggle("darkBody");
-    for (const logo of logos) {
-        logo.classList.toggle("darkLogo");
-    }
-    for (const textLink of textLinks) {
-        textLink.classList.toggle("darkText");
-    }
-    for (const button of buttons) {
-        button.classList.toggle("darkButton");
-    }
-    for (const project of projects) {
-        project.classList.toggle("darkEntry");
-    }
-}
 
 function showProjects() {
     this.classList.toggle("show");
     this.nextElementSibling.classList.toggle("show");
 }
 
-themeSwitcher.addEventListener("click", switchThemes);
 
 for (const project of projects) {
     project.addEventListener("click", showProjects);
